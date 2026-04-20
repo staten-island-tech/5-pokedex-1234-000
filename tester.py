678ddde98a6c483a2cd3842779ac643977b42559
1234-000 <wilsonm68@nycstudents.net>
2026-04-20 09:19:42 -0400

    s

--- tester.py
+++ tester.py
@@ -17,25 +17,53 @@ pokedex = open("./pokedex.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
 data = json.load(pokedex)
 
-#for i in range(0,809):
-    #print(data[i]["name"]) #name print
-#user = input("Language? English, Japanese, Chinese, or French ").lower() #Ask question
+moves = open("./moves.json", encoding="utf8")  
+data = json.load(moves)
 
-#for i in range(0,809):
-    #print(data[i]["name"][user]) #prints what language user wants 
-#for i in range(0,809):
-    #print(data[i]["name"][user]) 
+'''for i in range(0,809):
+    print(data[i]["name"]) #name print
+user = input("Language? English, Japanese, Chinese, or French ").lower() #Ask question
 
-type_ask = input("What Type? ")
+for i in range(0,809):
+    print(data[i]["name"][user]) #prints what language user wants 
+for i in range(0,809):
+    print(data[i]["name"][user]) 
 
+type_ask = input("What Type? ")'''
 
-for pokemon in data:
+'''for pokemon in data:
     if type_ask in pokemon["type"]:
         print(pokemon["name"]["english"])
         Found = True
     else: Found  = False
 if Found == False:
-    print("Nothing")
+    print("Nothing") '''
+
+'''count = 0
+name_ask = input("Name Match ")
+for char in data:
+    if name_ask in char["name"]["english"]:
+        print(char["name"]["english"])
+        count = 1
+if count == 0:
+    print("None Was Found")'''
+
+
+
+
+
+moves = input("Pokemon: ")
+for things in data:
+    if moves in things["name"]["english"]:
+        print(things["type"])
+
+
+    
+
+
+
+
+    
 
 
 
